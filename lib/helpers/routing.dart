5379434d5c11045d68dbb8cbc63f673ef65cfdeb
Revanth2002 <68279005+Revanth2002@users.noreply.github.com>
2022-02-28 17:27:14 +0530

    Changes made in routing.dart

--- lib/helpers/routing.dart
+++ lib/helpers/routing.dart
@@ -1,10 +1,10 @@
 
-const String PROTOCOL = "https";
+const String PROTOCOL = "http";
 const String PORT = "8000";
-const String DOMAIN = "192.168.1.15"; // acer
+const String DOMAIN = "192.168.1.9"; // acer
 // const String DOMAIN = "192.168.1.7";  // lenovo
 // const String BASE_URL = "$PROTOCOL://$DOMAIN:$PORT/api";
-const String BASE_URL = "$PROTOCOL://homietouch.loca.lt/api";
+const String BASE_URL =  "$PROTOCOL://$DOMAIN:$PORT/api/main/pat"; //"$PROTOCOL://homietouch.loca.lt/api";
 
 /*-------Home Screen API ------- */
 const String HOME_SCREEN_URL = "$BASE_URL/home-screen-get/";
